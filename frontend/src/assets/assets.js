ed77bea5dc2848ed74a0b572301d64321c59d470
Lytvynov Dmytro <70574498+IndifferentNetrunner@users.noreply.github.com>
2024-11-28 04:38:26 +0200

    Add Frontend with assets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,316 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Салат",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Роли",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Десерти",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Сендвіч",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Торт",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Паста",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Локшина",
+        menu_image: menu_7
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Грецький салат",
+        image: food_1,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Салат"
+    },
+    {
+        _id: "2",
+        name: "Овочевий салат",
+        image: food_2,
+        price: 18,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Салат"
+    },
+    {
+        _id: "3",
+        name: "Конюшиновий салат",
+        image: food_3,
+        price: 16,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Салат"
+    },
+    {
+        _id: "4",
+        name: "Курячий салат",
+        image: food_4,
+        price: 24,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Салат"
+    },
+    {
+        _id: "5",
+        name: "Рулети з лазаньї",
+        image: food_5,
+        price: 14,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Рулети"
+    },
+    {
+        _id: "6",
+        name: "Пері Пері Рулети",
+        image: food_6,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Рулети"
+    },
+    {
+        _id: "7",
+        name: "Курячі Рулети",
+        image: food_7,
+        price: 20,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Рулети"
+    },
+    {
+        _id: "8",
+        name: "Овочеві Рулети",
+        image: food_8,
+        price: 15,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Рулети"
+    },
+    {
+        _id: "9",
+        name: "Морозиво Ріпл",
+        image: food_9,
+        price: 14,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Десерти"
+    },
+    {
+        _id: "10",
+        name: "Фруктове Морозиво",
+        image: food_10,
+        price: 22,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Десерти"
+    },
+    {
+        _id: "11",
+        name: "Морозиво в банці",
+        image: food_11,
+        price: 10,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Десерти"
+    },
+    {
+        _id: "12",
+        name: "Ванільне Морозиво",
+        image: food_12,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Десерти"
+    },
+    {
+        _id: "13",
+        name: "Курячий Сендвіч",
+        image: food_13,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Сендвіч"
+    },
+    {
+        _id: "14",
+        name: "Веганський Сендвіч",
+        image: food_14,
+        price: 18,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Сендвіч"
+    },
+    {
+        _id: "15",
+        name: "Гриль Сендвіч",
+        image: food_15,
+        price: 16,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Сендвіч"
+    },
+    {
+        _id: "16",
+        name: "Сендвіч з хлібом",
+        image: food_16,
+        price: 24,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Сендвіч"
+    },
+    {
+        _id: "17",
+        name: "Кекс",
+        image: food_17,
+        price: 14,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Торт"
+    },
+    {
+        _id: "18",
+        name: "Веганський Торт",
+        image: food_18,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Торт"
+    },
+    {
+        _id: "19",
+        name: "Торт Бутерброд",
+        image: food_19,
+        price: 20,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Торт"
+    },
+    {
+        _id: "20",
+        name: "Скибочки Торту",
+        image: food_20,
+        price: 15,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Торт"
+    },
+    {
+        _id: "21",
+        name: "Сирна Паста",
+        image: food_25,
+        price: 12,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Паста"
+    },
+    {
+        _id: "22",
+        name: "Томатна Паста",
+        image: food_26,
+        price: 18,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Паста"
+    },
+    {
+        _id: "23",
+        name: "Кремова Паста",
+        image: food_27,
+        price: 16,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Паста"
+    },
+    {
+        _id: "24",
+        name: "Куряча Паста",
+        image: food_28,
+        price: 24,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Паста"
+    },
+    {
+        _id: "25",
+        name: "Масляна Локшина",
+        image: food_29,
+        price: 14,
+        description: "Їжа забезпечує необхідні поживні речовини для загального здоров'я та благополуччя",
+        category: "Локшина"
+    },
+]
